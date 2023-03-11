eb81b31093e0ac08e54ce29245739d46ce729831
Reddi-Umamahesh <125273222+Reddi-Umamahesh@users.noreply.github.com>
2023-03-11 12:59:28 +0530

    Add files via upload

---
+++ tower_of_hannoi.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class tower_of_hannoi {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void toh(int n,char helper,char source,char destination){
+        if(n==1){
+            System.out.println("move disc "+n+" from "+source+" to "+destination);
+        }
+        toh(n-1,,3,2);
+        System.out.println("move disc "+n+" from "+source+" to "+destination);
+        toh()
+    }
+    
+}
+
+
